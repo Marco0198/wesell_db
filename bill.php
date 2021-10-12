b91aaecd9c609ef5d4e1633af2c605f141e0d5d7
clarisse88 <clarampiana@gmail.com>
2021-10-13 00:37:28 +0200

    my changes

--- bill.php
+++ bill.php
@@ -1,37 +1,32 @@
 <?php
 include "db.php";
 
-    // if the form's submit button is clicked, we need to process the form
-    if (isset($_POST['woocommerce_checkout_place_order'])) {
-		// get variables from the billing form
+// if the form's submit button is clicked, we need to process the form
+if (isset($_POST['woocommerce_bill_insert'])) {
+    // get variables from the billing form
 
-		
-		$country = $_POST['billing_country'];
-        $firstname = $_POST['billing_first_name'];
-        $lastname = $_POST['billing_last_name'];
-        $address = $_POST['billing_address_1'];
-        $apartement = $_POST['billing_address_2'];
+    $country = $_POST['billing_country'];
+    $firstname = $_POST['billing_first_name'];
+    $lastname = $_POST['billing_last_name'];
+    $address = $_POST['billing_address_1'];
+    $apartement = $_POST['billing_address_2'];
 
-        $town = $_POST['billing_city'];
-        $province = $_POST['billing_state'];
-        $postcode = $_POST['billing_postcode'];
-        $email = $_POST['billing_email'];
-        $phonenumber = $_POST['billing_phone'];
+    $town = $_POST['billing_city'];
+    $province = $_POST['billing_state'];
+    $postcode = $_POST['billing_postcode'];
+    $email = $_POST['billing_email'];
+    $phonenumber = $_POST['billing_phone'];
 
-        //writesql query
-        $sql= "INSERT INTO bill (country, firstname, lastname, address, Town, province, postcode, email, phonenumber, apartment) VALUES ('$country','$firstname','$lastname','$address','$town','$province','$postcode','$email','$phonenumber','$apartement')";
-        // execute the query
+    //writesql query
+    $sql = "INSERT INTO bill (country, firstname, lastname, address, Town, province, postcode, email, phonenumber, apartment) VALUES ('$country','$firstname','$lastname','$address','$town','$province','$postcode','$email','$phonenumber','$apartement')";
+    // execute the query
 
-		$result = mysqli_query($con,$sql);
+    $result = mysqli_query($con, $sql);
 
-		if ($result == TRUE) {
-			echo "New record created successfully.";
-		}else{
-			echo "Error:". $sql . "<br>". $con->error;
-		}
-        $con->close();
+    if ($result == true) {
+        echo "New record created successfully.";
+    } else {
+        echo "Error:" . $sql . "<br>" . $con->error;
     }
-    
-
-
-?>
+    $con->close();
+}
